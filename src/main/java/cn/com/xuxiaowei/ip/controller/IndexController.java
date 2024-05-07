5bbc6cf7b4353aefd239dca1aea626545e481c11
徐晓伟 <xuxiaowei@xuxiaowei.com.cn>
2024-05-07 23:51:19 +0800

    :sparkles: 请求转发

---
+++ src/main/java/cn/com/xuxiaowei/ip/controller/IndexController.java
@@ -0,0 +1,22 @@
+package cn.com.xuxiaowei.ip.controller;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @author xuxiaowei
+ * @since 0.0.1
+ */
+@Slf4j
+@Controller
+public class IndexController {
+
+	@RequestMapping
+	public String index(HttpServletRequest request, HttpServletResponse response) {
+		return "forward:/ip";
+	}
+
+}
