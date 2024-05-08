f60359992a1865a62b69c3420b786359707286fe
徐晓伟 <xuxiaowei@xuxiaowei.com.cn>
2024-05-08 15:35:06 +0800

    :memo: 增加 WebService 接口说明

--- src/main/java/cn/com/xuxiaowei/ip/config/SpringdocConfig.java
+++ src/main/java/cn/com/xuxiaowei/ip/config/SpringdocConfig.java
@@ -18,6 +18,7 @@ import org.springframework.context.annotation.Configuration;
 				"<a target=\"_blank\" href=\"https://jihulab.com/xuxiaowei-jihu/xuxiaowei-com-cn/ip\">极狐 GitLab</a><br>" +
 				"<a target=\"_blank\" href=\"https://gitee.com/xuxiaowei-com-cn/ip\">Gitee</a><br>" +
 				"<a target=\"_blank\" href=\"https://github.com/xuxiaowei-com-cn/ip\">GitHub</a><br><br>" +
+				"接口支持 WebService：<a target=\"_blank\" href=\"/ws\">CXF WebService 列表</a><br><br>" +
 				"注意：<br>" + "本项目如果开启了内容协商，返回数据将遵守以下滚则。<br>" +
 				"根据请求头中的 Accept 匹配到 application/json，将返回 json 类型的数据。<br>" +
 				"根据请求头中的 Accept 匹配到 application/xml，将返回 xml 类型的数据。<br>" +
