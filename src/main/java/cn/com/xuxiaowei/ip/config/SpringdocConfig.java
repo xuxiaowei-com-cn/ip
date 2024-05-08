968c7f2b946932dc6c50d3c9b469d309db0d9fd0
徐晓伟 <xuxiaowei@xuxiaowei.com.cn>
2024-05-08 11:12:09 +0800

    :memo: a target _blank

--- src/main/java/cn/com/xuxiaowei/ip/config/SpringdocConfig.java
+++ src/main/java/cn/com/xuxiaowei/ip/config/SpringdocConfig.java
@@ -15,9 +15,9 @@ import org.springframework.context.annotation.Configuration;
 @OpenAPIDefinition(info = @Info(title = "IP 地理信息",
 		description = "根据 IP 获取：大陆、国家、省、市、网络运营商、网段、是否为欧盟国家等。<br><br>" +
 				"项目地址：<br>" +
-				"<a href=\"https://jihulab.com/xuxiaowei-jihu/xuxiaowei-com-cn/ip\">极狐 GitLab</a><br>" +
-				"<a href=\"https://gitee.com/xuxiaowei-com-cn/ip\">Gitee</a><br>" +
-				"<a href=\"https://github.com/xuxiaowei-com-cn/ip\">GitHub</a><br><br>" +
+				"<a target=\"_blank\" href=\"https://jihulab.com/xuxiaowei-jihu/xuxiaowei-com-cn/ip\">极狐 GitLab</a><br>" +
+				"<a target=\"_blank\" href=\"https://gitee.com/xuxiaowei-com-cn/ip\">Gitee</a><br>" +
+				"<a target=\"_blank\" href=\"https://github.com/xuxiaowei-com-cn/ip\">GitHub</a><br><br>" +
 				"注意：<br>" + "本项目如果开启了内容协商，返回数据将遵守以下滚则。<br>" +
 				"根据请求头中的 Accept 匹配到 application/json，将返回 json 类型的数据。<br>" +
 				"根据请求头中的 Accept 匹配到 application/xml，将返回 xml 类型的数据。<br>" +
